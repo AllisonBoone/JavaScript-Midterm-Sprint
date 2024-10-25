77f6c512c1e2d6d1d4d54053402e391ed995800e
Allison <Allison.Boone@keyin.com>
2024-10-25 18:40:09 -0230

    Written the first test to test typeof menuitem, properties, price range, dailyspecial, menu item of specific cuisine

--- __tests__/utils/weatherUtils.test.js
+++ __tests__/utils/weatherUtils.test.js
@@ -1,16 +1,46 @@
-const { Cuisines, Dishes } = require("../../utils/data");
-const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("../../utils/restaurantUtils");
+const { Cuisines, Dishes } = require('../../utils/data');
+const {
+  generateRandomMenuItem,
+  generateMenu,
+  selectRandomCuisine,
+} = require('../../utils/restaurantUtils');
 
+// Test got typeof menuItem, to check properties, price range, true/false of dailySpecial and generate menu item of specific cuisine.
 describe('Restaurant Functions', () => {
-    describe('generateRandomMenuItem', () => {
-      // Test implementations go here
+  describe('generateRandomMenuItem', () => {
+    // Test implementations go here
+    it('should return an item from the menu with valid properties', () => {
+      const cuisine = 'italian';
+      const menuItem = generateRandomMenuItem(cuisine);
+
+      expect(typeof menuItem).toBe('object');
+
+      expect(menuItem).toHaveProperty('name');
+      expect(menuItem).toHaveProperty('description');
+      expect(menuItem).toHaveProperty('price');
+      expect(menuItem).toHaveProperty('dailySpecial');
+
+      const price = parseFloat(menuItem.price);
+      expect(price).toBeGreaterThanOrEqual(5);
+      expect(price).toBeLesserThanOrEqual(25);
+
+      expect(typeof menuItem.dailySpecial).toBe('boolean');
     });
 
-    describe('generateMenu', () => {
-      // Test implementations go here
+    it('Should generate an item from the menu with a specific cuisine', () => {
+      const cuisine = 'italian';
+      const menuItem = generateRandomMenuItem(cuisine);
+      const cuisineDishes = Dishes[cuisine].map((dish) => dish.name);
+      expect(cuisineDishes).toContain(menuItem.name);
     });
+  });
 
-    describe('selectRandomCuisine', () => {
-      // Test implementations go here
-    });
+  // Test
+  describe('generateMenu', () => {
+    // Test implementations go here
+  });
+
+  describe('selectRandomCuisine', () => {
+    // Test implementations go here
+  });
 });
